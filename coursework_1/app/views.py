7e14c1a3ef66a8b734180891910dc659abca93c0
Adam Turner <ll16a6t@leeds.ac.uk>
2019-10-06 00:57:28 +0100

    Setup remaining templates and completed navbar functionality.

--- coursework_1/app/views.py
+++ coursework_1/app/views.py
@@ -4,7 +4,17 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template("page1.html",
+    return render_template("view_tasks.html",
                            title="Sample Page")
+
+@app.route('/completed')
+def display_completed():
+    return render_template("view_completed.html",
+                           title="Completed Tasks")
+
+@app.route('/add_task')
+def create_task():
+    return render_template("create_task.html",
+                           title="Create New Task.")
 
 
