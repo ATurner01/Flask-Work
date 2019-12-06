a78107d9b7f62f70f56bd5307215d05474362a73
Adam Turner <ll16a6t@leeds.ac.uk>
2019-12-06 23:59:25 +0000

    Setup templates and forms.

--- coursework_2/app/views.py
+++ coursework_2/app/views.py
@@ -1,7 +1,11 @@
-from flask import render_template
+from flask import render_template, flash
 from app import app
+from .forms import LoginForm
 
-@app.route('/')
-def index():
-    return "Hello World!!!"
+@app.route('/', methods=['GET', 'POST'])
+def Login():
+    form = LoginForm()
+    return render_template("login.html",
+                           title="Login",
+                           form=form)
 
