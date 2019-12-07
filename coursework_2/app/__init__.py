3c0eac60aafc50425a22495ca3e8b8522be270d6
Adam Turner <ll16a6t@leeds.ac.uk>
2019-12-07 00:13:55 +0000

    Implemented basic support for flask-login.

--- coursework_2/app/__init__.py
+++ coursework_2/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -7,5 +8,8 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 from app import views, models
--- coursework_2/app/models.py
+++ coursework_2/app/models.py
@@ -6,3 +6,24 @@ class User(db.Model):
     username = db.Column(db.String(32), index=True, unique=True)
     password = db.Column(db.String(64))
     email = db.Column(db.String(255), index=True, unique=True)
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def is_authenticated(self):
+        return self.authenticated
+
+    def is_active(self):
+        """Perhaps add account locking features later"""
+        return True
+
+    def is_anonymous(self):
+        """Perhaps add anonymous user functionality later"""
+        return False
+
+    def get_id(self):
+        """Need to convert user identifier to Unicode value"""
+        return chr(self.id)
+
+    def __repr__(self):
+        return "%s %s %s" % (self.id, self.username, self.email)
+
+
