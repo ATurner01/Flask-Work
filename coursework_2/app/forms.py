4e9301e5eb06b16556eea91e1f110fc4633721bf
Adam Turner <ll16a6t@leeds.ac.uk>
2019-12-11 23:58:02 +0000

    Added review form.

--- coursework_2/app/forms.py
+++ coursework_2/app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
-from wtforms import TextField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms import TextField, PasswordField, IntegerField
+from wtforms.validators import DataRequired, NumberRange, Email, Length
 
 
 class LoginForm(Form):
@@ -11,15 +11,17 @@ class LoginForm(Form):
 class RegisterForm(Form):
     username = TextField('username', validators=[DataRequired()])
     password = TextField('password', validators=[DataRequired()])
-    email = TextField('password', validators=[DataRequired()])
+    email = TextField('password', validators=[DataRequired(), Email()])
 
 
 class PasswordUpdateForm(Form):
     current_password = PasswordField('current_password', validators=[DataRequired()])
     new_password = PasswordField('new_password', validators=[DataRequired()])
     confirm_password = PasswordField('confirm_password', validators=[DataRequired()])
-    
-    
 
 
-    
+class ReviewForm(Form):
+    rating = IntegerField('rating', validators=[DataRequired(), NumberRange(min=0, max=10)])
+    message = TextField('message', validators=[Length(min=0, max=500)])
+
+
